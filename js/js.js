7298688bebea29ab4ae3fae008fecdf19ffc8989
Anhh <phanbuiducanh@gmail.com>
2025-04-19 08:27:56 +0700

    commit-19/04/2025

--- js/js.js
+++ js/js.js
@@ -961,3 +961,35 @@ document.addEventListener('DOMContentLoaded', () => {
       observer.observe(block);
     });
 });
+
+// ===== Text Animation ======================
+document.addEventListener('DOMContentLoaded', () => {
+    const txts = document.querySelectorAll('.txt-animation');
+  
+    const directions = ['from-top', 'from-right', 'from-left'];
+  
+    txts.forEach(txt => {
+      // Chọn random 1 hiệu ứng trong 3 hướng
+      const randomDirection = directions[Math.floor(Math.random() * directions.length)];
+      txt.classList.add(randomDirection);
+    });
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          setTimeout(() => {
+            entry.target.classList.add('show');
+          }, index * 150);
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+  
+    txts.forEach(txt => {
+      observer.observe(txt);
+    });
+});
+  
